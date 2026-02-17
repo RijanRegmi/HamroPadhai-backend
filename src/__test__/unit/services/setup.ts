f2cf0a36faf1df6d6be420f1f44f0f426f085351
Rijan Regmi <rijanregmi111@gmail.com>
2026-02-17 23:28:22 +0545

    update test

--- src/__test__/unit/services/setup.ts
+++ src/__test__/unit/services/setup.ts
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_TEST_URI =
-  process.env.MONGO_TEST_URI ||
-  "mongodb+srv://rijanregmi8_db_user:dbY2ZhoAm741ticA@qr.r1aggs9.mongodb.net/hamropadhai_test?retryWrites=true&w=majority&appName=Qr";
+const MONGO_TEST_URI = process.env.MONGO_TEST_URI as string;
+
+if (!MONGO_TEST_URI) {
+  throw new Error("MONGO_TEST_URI not defined");
+}
 
 export const connectDatabaseTest = async () => {
   if (mongoose.connection.readyState === 0) {
